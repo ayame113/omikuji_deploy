e8a093e11275823ab0cc75cf459d84ff3378a4d7
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-21 17:46:22 +0900

    omikuji

---
+++ serve.ts
@@ -0,0 +1,44 @@
+import { sample } from "https://deno.land/std@0.107.0/collections/mod.ts";
+
+const omikujis = [
+  Deno.env.get("おみくじ1"),
+  Deno.env.get("おみくじ2"),
+  Deno.env.get("おみくじ3"),
+  Deno.env.get("おみくじ4"),
+].filter((v) => v);
+const statement = Deno.env.get("文章");
+
+function getOmikuji() {
+  const result = sample(omikujis) ?? "おみくじが空でした"; //length===0
+  return `<h1>${
+    statement?.includes("%OMIKUJI%")
+      ? statement.replaceAll("%OMIKUJI%", result)
+      : result
+  }</h1>`;
+}
+
+async function handleHttp(conn: Deno.Conn) {
+  for await (const event of Deno.serveHttp(conn)) {
+    if (new URL(event.request.url).pathname === "/") {
+      event.respondWith(
+        new Response(getOmikuji(), {
+          status: 200,
+          headers: { "content-type": "text/html; charset=utf-8" },
+        }),
+      );
+      return;
+    }
+    event.respondWith(
+      new Response('404 Not Found <a href="/">top</a>', {
+        status: 404,
+        headers: {
+          "content-type": "text/plain",
+        },
+      }),
+    );
+  }
+}
+
+for await (const conn of Deno.listen({ port: 80 })) {
+  handleHttp(conn);
+}
