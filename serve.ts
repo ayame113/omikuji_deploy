4531d6e69ce7000e7912a893b7ed4639bcd88525
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-25 19:33:04 +0900

    use std/http

--- serve.ts
+++ serve.ts
@@ -1,3 +1,4 @@
+import { listenAndServe } from "https://deno.land/std@0.108.0/http/server.ts";
 import { sample } from "https://deno.land/std@0.107.0/collections/sample.ts";
 
 const omikujis = [
@@ -8,14 +9,18 @@ const omikujis = [
 ].filter((v) => v).map(escapeHTML);
 const sentence = escapeHTML(Deno.env.get("文章"));
 
-function escapeHTML(str?: string) {
-  return str
-    ?.replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
-}
+listenAndServe(":80", (request) => {
+  if (new URL(request.url).pathname === "/") {
+    return new Response(getOmikuji(), {
+      status: 200,
+      headers: { "content-type": "text/html; charset=utf-8" },
+    });
+  }
+  return new Response('404 Not Found <a href="/">top</a>', {
+    status: 404,
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+});
 
 function getOmikuji() {
   const result = sample(omikujis) ?? "おみくじが空でした。"; //length===0
@@ -27,28 +32,11 @@ function getOmikuji() {
   このおみくじは<a href="https://github.com/ayame113/omikuji_deploy">https://github.com/ayame113/omikuji_deploy</a>から作成されました。`;
 }
 
-async function handleHttp(conn: Deno.Conn) {
-  for await (const event of Deno.serveHttp(conn)) {
-    if (new URL(event.request.url).pathname === "/") {
-      event.respondWith(
-        new Response(getOmikuji(), {
-          status: 200,
-          headers: { "content-type": "text/html; charset=utf-8" },
-        }),
-      );
-      continue;
-    }
-    event.respondWith(
-      new Response('404 Not Found <a href="/">top</a>', {
-        status: 404,
-        headers: {
-          "content-type": "text/html; charset=utf-8",
-        },
-      }),
-    );
-  }
-}
-
-for await (const conn of Deno.listen({ port: 80 })) {
-  handleHttp(conn);
+function escapeHTML(str?: string) {
+  return str
+    ?.replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
 }
